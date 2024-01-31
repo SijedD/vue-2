12505c7aafa8494981c7139c2e494b5de426c819
gr421_vidol <Danilcarol56@gmail.com>
2024-01-31 17:26:34 +0700

    +2

--- js/main.js
+++ js/main.js
@@ -9,35 +9,29 @@ let app = new Vue({
     },
     methods: {
         addTask(){
-            this.needDoList.push(this.todo);
-        },
-        doCheck(){
-            for(let element in this.needDoList) {
-                    this.needDoList.splice(this.needDoList[element], 1)
+            if (this.needDoList.length >=3 ){
+                alert("Больше 3 нельзя")
             }
-                this.needDoListDo.push(this.todo)
-        },
-        doneCheck(){
-            for(let element in this.needDoListDo) {
-                this.needDoListDo.splice(this.needDoListDo[element], 1)
-            }
-            this.needDoListDone.push(this.todo)
+            else {
+            this.needDoList.push(this.todo);}
         },
         schetRadio() {
+            if (this.needDoListDo.length >=5){
+                alert("Больше 5 нельзя")
+            }
+            else {
             this.schetchik += 1
-            console.log(this.schetchik)
             if (this.schetchik >=3 && this.schetchik < 5){
                 for(let element in this.needDoList) {
                     this.needDoList.splice(this.needDoList[element], 1)
                 }
                 this.needDoListDo.push(this.todo)
                 this.schetchik = 0
-            }
+            }}
 
         },
         schetRadioDo(){
             this.schetchik += 1
-            console.log(this.schetchik)
             if (this.schetchik >= 2){
                 for(let element in this.needDoListDo) {
                     this.needDoListDo.splice(this.needDoListDo[element], 1)
