1e31f0c58406e355e7977cb5b5423a6068d77590
gr421_vidol <Danilcarol56@gmail.com>
2024-01-31 17:12:26 +0700

    +

--- js/main.js
+++ js/main.js
@@ -26,13 +26,26 @@ let app = new Vue({
         schetRadio() {
             this.schetchik += 1
             console.log(this.schetchik)
-            if(this.schetchik >= 3){
+            if (this.schetchik >=3 && this.schetchik < 5){
                 for(let element in this.needDoList) {
                     this.needDoList.splice(this.needDoList[element], 1)
                 }
                 this.needDoListDo.push(this.todo)
                 this.schetchik = 0
             }
+
+        },
+        schetRadioDo(){
+            this.schetchik += 1
+            console.log(this.schetchik)
+            if (this.schetchik >= 2){
+                for(let element in this.needDoListDo) {
+                    this.needDoListDo.splice(this.needDoListDo[element], 1)
+                }
+                this.schetchik = 0
+                this.needDoListDone.push(this.todo)
+            }
         }
+
     }})
 
