cc20efad538feafa58f6036ba68968336947f55e
gr421_vidol <Danilcarol56@gmail.com>
2024-01-31 18:25:30 +0700

    +3

--- js/main.js
+++ js/main.js
@@ -19,31 +19,35 @@ let app = new Vue({
             }
             else {
             this.needDoList.push(this.todo);
-            this.todo = ""}
+
+            }this.todo.value = ""
         },
         schetRadio() {
             if (this.needDoListDo.length >=5){
                 alert("Больше 5 нельзя")
+                return
             }
             else {
             this.schetchik += 1
             if (this.schetchik >=3 && this.schetchik < 5){
+                this.needDoListDo.push(this.todo)
+                this.schetchik = 0
                 for(let element in this.needDoList) {
                     this.needDoList.splice(this.needDoList[element], 1)
                 }
-                this.needDoListDo.push(this.todo)
-                this.schetchik = 0
+
             }}
 
         },
         schetRadioDo(){
             this.schetchik += 1
             if (this.schetchik >= 2){
+                this.schetchik = 0
+                this.needDoListDone.push(this.todo)
                 for(let element in this.needDoListDo) {
                     this.needDoListDo.splice(this.needDoListDo[element], 1)
                 }
-                this.schetchik = 0
-                this.needDoListDone.push(this.todo)
+
             }
         }
 
